9e21f0e1c9f33da59ce0e4a2d7e2cc4186ce5cde
TechnoMen <rohit.shri2389@gmail.com>
2019-10-21 23:47:12 +0530

    bug fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.static('build')); // myApp will be the same folder name.
 app.get('/*', function (req, res,next) {
 //  res.redirect('/'); 
-    res.sendFile('build/index.html', { root: __dirname });
+    res.sendFile('index.html', { root: __dirname });
 });
 app.listen(8080, 'localhost');
 console.log('MyProject Server is Listening on port 8080');
